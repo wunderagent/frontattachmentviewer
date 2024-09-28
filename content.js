d0f939cd162fe30a67861ee68340e6eb85d58480
marshalluous <m.stocker1987@gmail.com>
2024-09-27 23:14:52 -0300

    filtered by pdf and cleanup

--- content.js
+++ content.js
@@ -13,6 +13,15 @@ document.addEventListener('click', async function (event) {
     return;
   }
   url = url + '&embedded=true';
+
+  let pdfNameDiv = element.querySelector('div[class*="StyledNameDiv"]');
+  let pdfName = pdfNameDiv ? pdfNameDiv.textContent.trim() : null;
+  let fileExtension = pdfName ? pdfName.split('.').pop() : null;
+
+  if (fileExtension !== 'pdf') {
+    return;
+  }
+
   event.stopPropagation();
   event.preventDefault();
 
@@ -20,13 +29,9 @@ document.addEventListener('click', async function (event) {
 
   await chrome.runtime.sendMessage({ action: "fetchFile", fileUrl: "https://app.frontapp.com" + url },
     (response) => {
-      loading.remove();
       if (document.body.contains(loading)) {
-        console.log('Loading modal has been removed');
-      } else {
-        console.log('Loading modal is still present');
+        myShowModal(response.base64data)
       }
-      myShowModal(response.base64data)
     });
 
 }, true);
@@ -37,7 +42,7 @@ function showLoadingModal() {
     <div style="position:fixed; top: 0; left: 0; width: 100vw; height: 100vh; background: rgba(0,0,0,0.5); display: flex; justify-content: center; align-items: center;">
       <div style="display:flex; flex-direction: column; background: white; margin: 12rem 18rem; padding: 2rem; border-radius: 1rem; width: 100%; height: 90%;">
         <button id="closeModal" style="margin-bottom: 2rem; width: 8rem;">Close</button>
-        <p style="font-size: 2rem;">Loading...</p>
+        <div id="attachment-placeholder" style="width: 100%; flex-grow: 1; height: 100%;"><p style="font-size: 2rem;">Loading...</p></div>
       </div>
     </div>
   `;
@@ -48,62 +53,10 @@ function showLoadingModal() {
   });
 
   return modal;
-}
-
-function showLoadingModal2() {
-  const modal = document.createElement('div');
-  modal.innerHTML = `
-    <div style="position:fixed; top: 0; left: 0; width: 100vw; height: 100vh; background: rgba(0,0,0,0.5); display: flex; justify-content: center; align-items: center;">
-      <div style="display:flex; flex-direction: column; background: white; margin: 12rem 18rem; padding: 2rem; border-radius: 1rem; width: 100%; height: 90%;">
-        <button id="closeModal" style="margin-bottom: 2rem; width: 8rem;">Close</button>
-        <div id="attachment-placeholder"><p style="font-size: 2rem;">Loading...</p></div>
-      </div>
-    </div>
-  `;
-  document.body.appendChild(modal);
-
-  document?.getElementById('closeModal')?.addEventListener('click', function () {
-    modal.remove();
-  });
-
-  return modal;
-}
-
-
-function myShowModal2(base64Pdf) {
-  console.log('Modal script executed');
-  console.log("base64 string: " + base64Pdf);
-  let modal = document.createElement('div');
-  const byteCharacters = atob(base64Pdf.split(",")[1]);
-  // const byteCharacters = atob(base64Pdf);
-  const byteNumbers = new Array(byteCharacters.length);
-  for (let i = 0; i < byteCharacters.length; i++) {
-    byteNumbers[i] = byteCharacters.charCodeAt(i);
-  }
-  const byteArray = new Uint8Array(byteNumbers);
-  const blob = new Blob([byteArray], { type: 'application/pdf' });
-  const blobUrl = URL.createObjectURL(blob);
-
-  modal.innerHTML = `
-    <div style="position:fixed; top: 0; left: 0; width: 100vw; height: 100vh; background: rgba(0,0,0,0.5); display: flex; justify-content: center; align-items: center;">
-      <div style="display:flex; flex-direction: column; background: white; margin: 12rem 18rem; padding: 2rem; border-radius: 1rem; width: 100%; height: 90%;">
-        <button id="closeModal" style="margin-bottom: 2rem; width: 8rem;">Close</button>
-        <object id="pdf" data="${blobUrl}" type="application/pdf" style="width: 100%; flex-grow: 1;"></object>
-      </div>
-    </div>
-  `;
-  document.body.appendChild(modal);
-
-  document?.getElementById('closeModal')?.addEventListener('click', function () {
-    modal.remove();
-  });
 }
 
 
 function myShowModal(base64Pdf) {
-  console.log('Modal script executed');
-  console.log("base64 string: " + base64Pdf);
-  let modal = document.createElement('div');
   const byteCharacters = atob(base64Pdf.split(",")[1]);
   // const byteCharacters = atob(base64Pdf);
   const byteNumbers = new Array(byteCharacters.length);
@@ -114,17 +67,8 @@ function myShowModal(base64Pdf) {
   const blob = new Blob([byteArray], { type: 'application/pdf' });
   const blobUrl = URL.createObjectURL(blob);
 
-  modal.innerHTML = `
-    <div style="position:fixed; top: 0; left: 0; width: 100vw; height: 100vh; background: rgba(0,0,0,0.5); display: flex; justify-content: center; align-items: center;">
-      <div style="display:flex; flex-direction: column; background: white; margin: 12rem 18rem; padding: 2rem; border-radius: 1rem; width: 100%; height: 90%;">
-        <button id="closeModal" style="margin-bottom: 2rem; width: 8rem;">Close</button>
-        <object id="pdf" data="${blobUrl}" type="application/pdf" style="width: 100%; flex-grow: 1;"></object>
-      </div>
-    </div>
+  const placeholder = document?.getElementById("attachment-placeholder");
+  placeholder.innerHTML = `
+    <object id="pdf" data="${blobUrl}" type="application/pdf" style="width: 100%; height: 100%;"></object>
   `;
-  document.body.appendChild(modal);
-
-  document?.getElementById('closeModal')?.addEventListener('click', function () {
-    modal.remove();
-  });
 }
