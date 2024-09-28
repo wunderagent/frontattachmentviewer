5d4ec7b5e1f5049ae01e44641b9ebbb90a65a002
marshalluous <m.stocker1987@gmail.com>
2024-09-27 22:24:20 -0300

    working

--- background.js
+++ background.js
@@ -1,48 +1,58 @@
-chrome.runtime.onMessage.addListener(
-  async (message) => {
-    let currentTab = await getCurrentTab();
-    if (message.url && currentTab?.id) {
-      chrome.scripting.executeScript({
-        target: {tabId: currentTab.id, allFrames: true},
-        // files: ['modal.js'],
-        injectImmediately: true,
-        function: myShowModal,
-        args: [message.url]
-      }, () => {
-        if (chrome.runtime.lastError) {
-          console.error(chrome.runtime.lastError.message);
-        } else {
-          console.log('Script injected successfully');
-        }
-      });
-    }
-    return true;
-  }
-);
+const handleRequest = async (sendResponse, url) => {
+  const response = await fetch(url);
+  const fileBlob = await response.blob();
 
-async function getCurrentTab() {
-  let queryOptions = { active: true, lastFocusedWindow: true };
-  // `tab` will either be a `tabs.Tab` instance or `undefined`.
-  let [tab] = await chrome.tabs.query(queryOptions);
-  return tab;
-}
+  // Convert Blob to Base64
+  const reader = new FileReader();
+  reader.onloadend = () => {
+    const base64data = reader.result;
+    sendResponse({ success: true, base64data });
+  };
+  reader.readAsDataURL(fileBlob); // Read file as Base64
+};
 
-async function myShowModal(pdfUrl) {
-  console.log('Modal script executed');
-  let modal = document.createElement('div');
-  modal.innerHTML = `
-    <div style="position:fixed; top: 0; left: 0; width: 100%; height: 100%; background: rgba(0,0,0,0.5); display: flex; justify-content: center; align-items: center;">
-    <div style="background: white; padding: 20px; border-radius: 10px; max-width: 600px;">
-        <button id="closeModal">Close</button>
-        <object type="application/pdf" data="https://app.frontapp.com${pdfUrl}" width="100%" height="500px">
-        </object>
-        <embed src="https://app.frontapp.com${pdfUrl}" type="application/pdf" width="100%" height="100%">
-      </div>
-    </div>
-  `;
-  document.body.appendChild(modal);
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  const url = request.fileUrl;
+  handleRequest(sendResponse, url) // makes sure that the return statement is not being sent before this method is executed
+  return true; // Indicates that sendResponse will be called asynchronously
+});
 
-  document?.getElementById('closeModal')?.addEventListener('click', function () {
-    modal.remove();
-  });
-}
+// async function getCurrentTab() {
+//   let queryOptions = { active: true, lastFocusedWindow: true };
+//   // `tab` will either be a `tabs.Tab` instance or `undefined`.
+//   let [tab] = await chrome.tabs.query(queryOptions);
+//   return tab;
+// }
+
+// const bla = (response) => {
+//   // Create an iframe to display the file
+//   const iframe = document.createElement("iframe");
+//   iframe.src = response;
+//   iframe.style.width = "100%";
+//   iframe.style.height = "100vh";
+  
+//   document.body.appendChild(iframe);
+  
+//   // Optionally remove the iframe after some time or based on user action
+//   setTimeout(() => {
+//     iframe.remove();
+//   }, 60000); // Example: after 60 seconds
+// }
+
+// function myShowModal(pdfUrl) {
+//   console.log('Modal script executed');
+//   let modal = document.createElement('div');
+//   modal.innerHTML = `
+//     <div style="position:fixed; top: 0; left: 0; width: 100%; height: 100vh; background: rgba(0,0,0,0.5); display: flex; justify-content: center; align-items: center;">
+//       <div style="background: white; padding: 20px; border-radius: 10px; max-width: 600px;">
+//         <button id="closeModal">Close</button>
+//         <iframe width="100%" height="100%" src="${pdfUrl}"></iframe>
+//       </div>
+//     </div>
+//   `;
+//   document.body.appendChild(modal);
+
+//   document?.getElementById('closeModal')?.addEventListener('click', function () {
+//     modal.remove();
+//   });
+// }
--- content.js
+++ content.js
@@ -16,36 +16,115 @@ document.addEventListener('click', async function (event) {
   event.stopPropagation();
   event.preventDefault();
 
-  // Show dialog/modal
-  // myShowModal(url);
-  chrome.runtime.sendMessage({action: "showModal", url: url});
+  const loading = showLoadingModal();
+
+  await chrome.runtime.sendMessage({ action: "fetchFile", fileUrl: "https://app.frontapp.com" + url },
+    (response) => {
+      loading.remove();
+      if (document.body.contains(loading)) {
+        console.log('Loading modal has been removed');
+      } else {
+        console.log('Loading modal is still present');
+      }
+      myShowModal(response.base64data)
+    });
+
 }, true);
 
-const toJson = (target) => !target ? JSON.stringify({}) : JSON.stringify({
-  tagName: target.tagName,
-  attributes: Array.from(target.attributes).reduce((acc, currAttr) => {
-    acc[currAttr.name] = currAttr.value;
-    return acc;
-  }, {})
-});
+function showLoadingModal() {
+  const modal = document.createElement('div');
+  modal.innerHTML = `
+    <div style="position:fixed; top: 0; left: 0; width: 100vw; height: 100vh; background: rgba(0,0,0,0.5); display: flex; justify-content: center; align-items: center;">
+      <div style="display:flex; flex-direction: column; background: white; margin: 12rem 18rem; padding: 2rem; border-radius: 1rem; width: 100%; height: 90%;">
+        <button id="closeModal" style="margin-bottom: 2rem; width: 8rem;">Close</button>
+        <p style="font-size: 2rem;">Loading...</p>
+      </div>
+    </div>
+  `;
+  document.body.appendChild(modal);
+
+  document?.getElementById('closeModal')?.addEventListener('click', function () {
+    modal.remove();
+  });
+
+  return modal;
+}
+
+function showLoadingModal2() {
+  const modal = document.createElement('div');
+  modal.innerHTML = `
+    <div style="position:fixed; top: 0; left: 0; width: 100vw; height: 100vh; background: rgba(0,0,0,0.5); display: flex; justify-content: center; align-items: center;">
+      <div style="display:flex; flex-direction: column; background: white; margin: 12rem 18rem; padding: 2rem; border-radius: 1rem; width: 100%; height: 90%;">
+        <button id="closeModal" style="margin-bottom: 2rem; width: 8rem;">Close</button>
+        <div id="attachment-placeholder"><p style="font-size: 2rem;">Loading...</p></div>
+      </div>
+    </div>
+  `;
+  document.body.appendChild(modal);
+
+  document?.getElementById('closeModal')?.addEventListener('click', function () {
+    modal.remove();
+  });
+
+  return modal;
+}
 
 
-// function myShowModal(pdfUrl) {
-//   console.log('Modal script executed');
-//   let modal = document.createElement('div');
-//   modal.innerHTML = `
-//     <div style="position:fixed; top: 0; left: 0; width: 100%; height: 100%; background: rgba(0,0,0,0.5); display: flex; justify-content: center; align-items: center;">
-//       <div style="background: white; padding: 20px; border-radius: 10px; max-width: 600px;">
-//         <button id="closeModal">Close</button>
-//         <object type="application/pdf" data="https://app.frontapp.com${pdfUrl}" width="100%" height="500px">
-//         </object>
-//         <embed src="https://app.frontapp.com${pdfUrl}" type="application/pdf" width="100%" height="100%">
-//       </div>
-//     </div>
-//   `;
-//   document.body.appendChild(modal);
+function myShowModal2(base64Pdf) {
+  console.log('Modal script executed');
+  console.log("base64 string: " + base64Pdf);
+  let modal = document.createElement('div');
+  const byteCharacters = atob(base64Pdf.split(",")[1]);
+  // const byteCharacters = atob(base64Pdf);
+  const byteNumbers = new Array(byteCharacters.length);
+  for (let i = 0; i < byteCharacters.length; i++) {
+    byteNumbers[i] = byteCharacters.charCodeAt(i);
+  }
+  const byteArray = new Uint8Array(byteNumbers);
+  const blob = new Blob([byteArray], { type: 'application/pdf' });
+  const blobUrl = URL.createObjectURL(blob);
 
-//   document?.getElementById('closeModal')?.addEventListener('click', function () {
-//     modal.remove();
-//   });
-// }
+  modal.innerHTML = `
+    <div style="position:fixed; top: 0; left: 0; width: 100vw; height: 100vh; background: rgba(0,0,0,0.5); display: flex; justify-content: center; align-items: center;">
+      <div style="display:flex; flex-direction: column; background: white; margin: 12rem 18rem; padding: 2rem; border-radius: 1rem; width: 100%; height: 90%;">
+        <button id="closeModal" style="margin-bottom: 2rem; width: 8rem;">Close</button>
+        <object id="pdf" data="${blobUrl}" type="application/pdf" style="width: 100%; flex-grow: 1;"></object>
+      </div>
+    </div>
+  `;
+  document.body.appendChild(modal);
+
+  document?.getElementById('closeModal')?.addEventListener('click', function () {
+    modal.remove();
+  });
+}
+
+
+function myShowModal(base64Pdf) {
+  console.log('Modal script executed');
+  console.log("base64 string: " + base64Pdf);
+  let modal = document.createElement('div');
+  const byteCharacters = atob(base64Pdf.split(",")[1]);
+  // const byteCharacters = atob(base64Pdf);
+  const byteNumbers = new Array(byteCharacters.length);
+  for (let i = 0; i < byteCharacters.length; i++) {
+    byteNumbers[i] = byteCharacters.charCodeAt(i);
+  }
+  const byteArray = new Uint8Array(byteNumbers);
+  const blob = new Blob([byteArray], { type: 'application/pdf' });
+  const blobUrl = URL.createObjectURL(blob);
+
+  modal.innerHTML = `
+    <div style="position:fixed; top: 0; left: 0; width: 100vw; height: 100vh; background: rgba(0,0,0,0.5); display: flex; justify-content: center; align-items: center;">
+      <div style="display:flex; flex-direction: column; background: white; margin: 12rem 18rem; padding: 2rem; border-radius: 1rem; width: 100%; height: 90%;">
+        <button id="closeModal" style="margin-bottom: 2rem; width: 8rem;">Close</button>
+        <object id="pdf" data="${blobUrl}" type="application/pdf" style="width: 100%; flex-grow: 1;"></object>
+      </div>
+    </div>
+  `;
+  document.body.appendChild(modal);
+
+  document?.getElementById('closeModal')?.addEventListener('click', function () {
+    modal.remove();
+  });
+}
